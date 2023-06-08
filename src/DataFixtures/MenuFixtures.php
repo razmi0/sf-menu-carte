8673daf665a5992075f5592322cc5db327006daa
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-06-08 23:27:00 +0200

    hard fixture

--- src/DataFixtures/MenuFixtures.php
+++ src/DataFixtures/MenuFixtures.php
@@ -11,30 +11,43 @@ class MenuFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $name1 = 'Menu Midi Entrée/Plat';
-        $name2 = 'Menu Midi Plat/Dessert';
-        $name3 = 'Menu Enfant Entrée/Plat/Dessert';
+        $name1 = 'Menu Midi';
+        $name2 = 'Menu Soir Week-End';
+        $name3 = 'Menu Enfant';
+        $name4 = 'Menu A La Carte';
 
-        // $name1 = 'Menu Midi Entrée/Plat';
+        // $name1 = 'Menu Midi ';
         $menu = new Menu();
         $menu->setName($name1);
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_1));
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_2));
+        $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_3));
         $manager->persist($menu);
 
-        // $name2 = 'Menu Midi Plat/Dessert';
+        // $name2 = 'Menu Soir Week-End';
         $menu = new Menu();
         $menu->setName($name2);
+        $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_1));
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_2));
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_3));
         $manager->persist($menu);
 
-        // $name3 = 'Menu Enfant Entrée/Plat/Dessert';
+        // $name3 = 'Menu Enfant';
         $menu = new Menu();
         $menu->setName($name3);
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_1));
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_2));
         $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_3));
+        $manager->persist($menu);
+
+        // $name4 = 'Menu A La Carte';
+        $menu = new Menu();
+        $menu->setName($name4);
+        $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_1));
+        $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_2));
+        $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_3));
+        $menu->addCategory($this->getReference(CategoryFixtures::CATEGORY_REFERENCE_5));
+
         $manager->persist($menu);
 
         $manager->flush();
