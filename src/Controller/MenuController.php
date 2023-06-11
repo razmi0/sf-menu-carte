62cac79d01d4f0899882958241fb39fd97556a6a
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-06-11 13:20:41 +0200

    clean

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\ItemRepository;
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class MenuController extends AbstractController
 {
-    #[Route('/menu', name: 'app_menu')]
-    public function index(): Response
+    #[Route('/menu', name: 'app_menu', methods: ['HEAD','GET'])]
+    public function index(ItemRepository $itemRepository, CategoryRepository $categoryRepository, MenuRepository $menuRepository): Response
     {
+        //MenuMidi
+        //--
+
+        //MenuSoir
+        //--
+
+        //MenuEnfant
+        //--
+
+        //Menu A la carte
+        //--
+
+        
+        // dd($menus);
+        $items = $itemRepository->findAll();
+        $categories = $categoryRepository->findAll();
+        $menus = $menuRepository->findAll();
+        dump($menus);
+
+        // Je veux acceder aux items de chaque menu
+       
+
+
+       
+
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
+            'items' => $items,
+            'categories' => $categories,
+            'menus' => $menus,
         ]);
     }
 }
